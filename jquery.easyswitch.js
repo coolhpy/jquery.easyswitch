a66c65ea2dba2c87c2d77cf372311159711d5c96
Hpyer <coolhpy@163.com>
2015-03-26 16:40:43 +0800

    增加IE6+的兼容性

--- jquery.easyswitch.js
+++ jquery.easyswitch.js
@@ -49,9 +49,9 @@ USAGE:
 				opts[k] = (custom == undefined ? v : custom);
 			});
 
-			html = '<span class="'+opts['class-slider']+'"></span>\
-					<span class="'+opts['class-label']+'">'+opts['label-on']+'</span>\
-					<span class="'+opts['class-label']+'">'+opts['label-off']+'</span>';
+			html = '<span class="'+opts['class-label']+'">'+opts['label-on']+'</span>\
+					<span class="'+opts['class-label']+'">'+opts['label-off']+'</span>\
+					<span class="'+opts['class-slider']+'"></span>';
 			if (opts['name']) {
 				html += '<input type="hidden" id="easyswitch-'+opts['name']+'" name="'+opts['name']+'" value="" />';
 			}
@@ -62,24 +62,31 @@ USAGE:
 			}
 
 			self.append(html).click(function() {
-				var value;
 				if (self.hasClass(opts['class-off'])) {
-					self.removeClass(opts['class-off']).addClass(opts['class-on']);
-					value = 1;
+					self.find('.'+opts['class-slider']).animate({left:'50%'}, 'fast', function() {
+						self.removeClass(opts['class-off']).addClass(opts['class-on']);
+						var value = 1;
+						$('#easyswitch-'+opts['name']).val(value);
+						opts['callback'](value, self);
+					});
 				} else {
-					self.removeClass(opts['class-on']).addClass(opts['class-off']);
-					value = 0;
+					self.find('.'+opts['class-slider']).animate({left:'1px'}, 'fast', function() {
+						self.removeClass(opts['class-on']).addClass(opts['class-off']);
+						var value = 0;
+						$('#easyswitch-'+opts['name']).val(value);
+						opts['callback'](value, self);
+					});
 				}
-				$('#easyswitch-'+opts['name']).val(value);
-				opts['callback'](value, self);
 			});
 
 			if (opts['default']) {
 				self.addClass(opts['class-on']);
 				$('#easyswitch-'+opts['name']).val(1);
+				self.find('.'+opts['class-slider']).css({left:'50%'});
 			} else {
 				self.addClass(opts['class-off']);
 				$('#easyswitch-'+opts['name']).val(0);
+				self.find('.'+opts['class-slider']).css({left:'1px'});
 			}
 		});
 	}
